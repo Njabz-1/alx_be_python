6213de21db80500513195719a3ed42d493507f45
Njabulo <mhlambinjabulo@gmail.com>
2024-06-23 15:24:09 +0200

    Adding task 2 and 3 to the repo

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,8 @@
+# Assign value to the variable
+hours = 2  # Number of hours
+
+# Calculate the number of seconds in the given hours
+seconds = hours * 3600  # 3600 seconds per hour
+
+# Print the result
+print(f"{hours} hour(s) is {seconds} seconds.")
