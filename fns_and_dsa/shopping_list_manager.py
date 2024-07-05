29c9ad9d9720095a882c7fab55953202291179ee
Njabulo <mhlambinjabulo@gmail.com>
2024-07-05 21:53:27 +0200

    Change wording

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -12,11 +12,11 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            item = input("Enter the item name to add: ")
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
             print(f"{item} has been added to the list.")
         elif choice == '2':
-            item = input("Enter the item name to remove: ")
+            item = input("Enter the item to remove: ")
             if item in shopping_list:
                 shopping_list.remove(item)
                 print(f"{item} has been removed from the list.")
