367a67252c210b63f48fd92fbaa8895fe9d81206
Njabulo <mhlambinjabulo@gmail.com>
2024-07-14 22:33:32 +0200

    Add string formatting

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -5,7 +5,7 @@ def safe_divide(numerator, denominator):
         if denom == 0:
             return "Error: Cannot divide by zero."
         result = num / denom
-        return f"The result of the division is {result}."
+        return f"The result of the division is {result}"
     except ValueError:
         return "Error: Please enter numeric values only."
     except ZeroDivisionError:
